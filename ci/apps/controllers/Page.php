811940632dc1fb30421cfc04bf9cb5ef7dfa94de
Siegleind <matsu.yoshioka@sushitei.co.id>
2020-03-27 11:05:48 +0700

    Initial Page Controller
    Untuk pembuatan halaman dinamis

---
+++ ci/apps/controllers/Page.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Page extends SISTER_Controller
+{
+    /**
+    ==========================
+
+    Dibawah Ini Publik Class
+
+    ==========================
+    **/
+
+    public $skipCheck = array('index');
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->session->isLoggedIn(true);
+    }
+    
+    public function view($page='test_aja')
+    {
+        #exit(print_r($_SESSION));
+        $this->load->helper('html','url');
+        $this->load->model('Page_model', 'pm');
+        $data['content']['body'] = $this->pm->detail(['page_url' => $page]);
+        if(is_array($data['content']['body'])){
+            $data['body']['page'] = 'Dashboard';
+            $data['body']['session'] = $this->state;
+            $data['body']['option']['stylesheet'][0] = base_url().'assets/modules/DataTables/dataTables.bootstrap4.min.css';
+            $data['body']['option']['js'][2] = base_url().'assets/scripts/portal/portal.js';
+            $data['body']['content'] = $this->load->view('portal/contents/page/view', $data['content'], TRUE);
+            $this->load->view('portal/templates/sufee/template', $data['body']);
+        }else{
+            echo 'Ndak ada ei';
+        }
+        
+        
+        #print_r($this->db->last_query());
+        
+    }
+}
